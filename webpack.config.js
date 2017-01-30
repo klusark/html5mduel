320b0a5455f4425688ebf14680c00ae085e06cb9
Joel Teichroeb <joel@teichroeb.net>
2017-01-30 14:41:28 -0800

    foo

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,13 @@
 module.exports = {
     resolve: {
             // Add '.ts' and '.tsx' as resolvable extensions.
-                    extensions: ["", ".webpack.js", ".web.js", ".ts", ".tsx", ".js"]
+                    extensions: [".webpack.js", ".web.js", ".ts", ".tsx", ".js"]
                         },
     devtool: "source-map",
     module: {
         loaders: [
             // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
             { test: /\.tsx?$/, loader: "awesome-typescript-loader" }
-        ],
-
-        preLoaders: [
-            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-            { test: /\.js$/, loader: "source-map-loader" }
         ]
     },
   entry: './js/client.ts',
